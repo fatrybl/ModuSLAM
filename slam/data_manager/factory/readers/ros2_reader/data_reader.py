5d9d259a5c560f5c420ce66aabb3a3099e6e7011
FelipeZerokun <epilefrojasleon@gmail.com>
2024-04-24 19:05:36 +0200

    Using rosbags_reader to get a list with the available elements in the rosbag and a table with all the data. Using the list and the table, the Elements can be created. Still need to properly create Sensors instances

--- slam/data_manager/factory/readers/ros2_reader/data_reader.py
+++ slam/data_manager/factory/readers/ros2_reader/data_reader.py
@@ -1,32 +1,28 @@
+import os
 from pathlib import Path
 
 from slam.data_manager.factory.data_reader_ABC import DataReader
-from slam.data_manager.factory.element import Element
-from slam.data_manager.factory.readers.ros2_reader.ros2_reader_state import Ros2ReaderState
+from slam.data_manager.factory.element import Element, RawMeasurement
+from slam.data_manager.factory.readers.kaist.auxiliary_classes import Location
+from slam.data_manager.factory.readers.ros2_reader.rosbags_reader import RosbagsManager
+from slam.setup_manager.sensors_factory.factory import SensorsFactory
+from slam.setup_manager.sensors_factory.sensors import Sensor
 from slam.system_configs.system.data_manager.batch_factory.datasets.kaist.config import Ros2Config
 from slam.system_configs.system.data_manager.batch_factory.regime import (
     Stream,
     TimeLimit,
 )
+from slam.system_configs.system.setup_manager.sensors import SensorConfig
 
 
 class Ros2DataReader(DataReader):
     """Data reader for ROS2 in test."""
 
     def __init__(self, regime: TimeLimit | Stream, dataset_params: Ros2Config):
-        data_stamp_file: Path = dataset_params.directory / dataset_params.data_stamp_file
-        csv_files_table: dict[str, Path] = dataset_params.csv_files_table
-        lidar_data_dirs_table: dict[str, Path] = dataset_params.lidar_data_dir_table
-        stereo_data_dirs_table: dict[str, Path] = dataset_params.stereo_data_dir_table
         self._dataset_directory: Path = dataset_params.directory
         self._regime = regime
 
-        self._reader_state = Ros2ReaderState(data_stamp_file, csv_files_table)
 
-        self._apply_dataset_dir(
-            root_dir=dataset_params.directory,
-            tables=(csv_files_table, lidar_data_dirs_table, stereo_data_dirs_table),
-        )
 
     @staticmethod
     def _apply_dataset_dir(root_dir: Path, tables: tuple[dict[str, Path], ...]) -> None:
@@ -65,3 +61,37 @@ class Ros2DataReader(DataReader):
 
         except:
             print("Could not read the data")
+
+
+
+
+if __name__ == "__main__":
+
+    print("Testing ROS2 data reader")
+    folder_path = Path(os.environ['DATA_DIR'])
+    bag_path = Path('{}/rosbag2_2024_1713944720'.format(folder_path))
+
+    first_sensor_config = SensorConfig("left_camera_rgba")
+    sensor_factory = SensorsFactory()
+
+    first_sensor = Sensor(first_sensor_config)
+    first_sensor_loc = Location()
+    test_measurement = RawMeasurement(sensor=first_sensor, values=10)
+
+    rb_manager = RosbagsManager(bag_path)
+    topic_list, data_table = rb_manager.rosbag_read(num_readings=20)
+
+    for topic in topic_list:
+        print(topic.split('/'))
+
+    for element in data_table:
+        topic = element[1].split('/')[-1]
+
+
+
+
+    timelimit = TimeLimit(start=10, stop=20)
+    ros2_config = Ros2Config(directory=bag_path)
+    Ros2DataReader(regime=timelimit, dataset_params=ros2_config)
+
+    
