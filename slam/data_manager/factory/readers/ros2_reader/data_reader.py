0d95ac1dd977df9726fa34533054eda76b31f50a
FelipeZerokun <epilefrojasleon@gmail.com>
2024-05-07 12:35:54 +0200

    testing Sensors Factory to setup imu sensor

--- slam/data_manager/factory/readers/ros2_reader/data_reader.py
+++ slam/data_manager/factory/readers/ros2_reader/data_reader.py
@@ -7,12 +7,17 @@ from slam.data_manager.factory.readers.kaist.auxiliary_classes import Location
 from slam.data_manager.factory.readers.ros2_reader.rosbags_reader import RosbagsManager
 from slam.setup_manager.sensors_factory.factory import SensorsFactory
 from slam.setup_manager.sensors_factory.sensors import Sensor
-from slam.system_configs.system.data_manager.batch_factory.datasets.kaist.config import Ros2Config
+from slam.system_configs.system.data_manager.batch_factory.datasets.kaist.config import (
+    Ros2Config,
+)
 from slam.system_configs.system.data_manager.batch_factory.regime import (
     Stream,
     TimeLimit,
 )
-from slam.system_configs.system.setup_manager.sensors import SensorConfig
+from slam.system_configs.system.setup_manager.sensors import (
+    SensorConfig,
+    SensorFactoryConfig,
+)
 
 
 class Ros2DataReader(DataReader):
@@ -21,13 +26,6 @@ class Ros2DataReader(DataReader):
     def __init__(self, regime: TimeLimit | Stream, dataset_params: Ros2Config):
         self._dataset_directory: Path = dataset_params.directory
         self._regime = regime
-
-
-
-    @staticmethod
-    def _apply_dataset_dir(root_dir: Path, tables: tuple[dict[str, Path], ...]) -> None:
-        for table in tables:
-            [table.update({sensor_name: root_dir / path}) for sensor_name, path in table.items()]
 
 
     def get_element(self) -> Element | None:
@@ -63,18 +61,17 @@ class Ros2DataReader(DataReader):
             print("Could not read the data")
 
 
-
-
 if __name__ == "__main__":
 
     print("Testing ROS2 data reader")
-    folder_path = Path(os.environ['DATA_DIR'])
-    bag_path = Path('{}/rosbag2_2024_1713944720'.format(folder_path))
+    folder_path = Path(os.environ["DATA_DIR"])
+    bag_path = Path("{}/rosbag2_2024_1713944720".format(folder_path))
 
-    first_sensor_config = SensorConfig("left_camera_rgba")
-    sensor_factory = SensorsFactory()
+    sensor_config = SensorConfig(name="test_sensor", type_name=Sensor.__name__)
+    factory_config = SensorFactoryConfig(sensors={sensor_config.name: sensor_config})
+    SensorsFactory.init_sensors(factory_config)
 
-    first_sensor = Sensor(first_sensor_config)
+    first_sensor = SensorsFactory.get_sensor("imu")
     first_sensor_loc = Location()
     test_measurement = RawMeasurement(sensor=first_sensor, values=10)
 
@@ -82,16 +79,11 @@ if __name__ == "__main__":
     topic_list, data_table = rb_manager.rosbag_read(num_readings=20)
 
     for topic in topic_list:
-        print(topic.split('/'))
+        print(topic.split("/"))
 
     for element in data_table:
-        topic = element[1].split('/')[-1]
-
-
-
+        topic = element[1].split("/")[-1]
 
     timelimit = TimeLimit(start=10, stop=20)
     ros2_config = Ros2Config(directory=bag_path)
     Ros2DataReader(regime=timelimit, dataset_params=ros2_config)
-
-    
