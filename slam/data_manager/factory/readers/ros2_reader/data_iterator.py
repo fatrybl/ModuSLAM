89ea7f22f0cc889bebb823987b55ebcb91282bed
FelipeZerokun <epilefrojasleon@gmail.com>
2024-05-09 18:36:00 +0200

    Data class method used to remember the last element obtained from the Rosbag file, and get the next one or reset the position back to 0

--- slam/data_manager/factory/readers/ros2_reader/data_iterator.py
+++ slam/data_manager/factory/readers/ros2_reader/data_iterator.py
@@ -1,29 +1,43 @@
-from dataclasses import dataclass, field
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+from slam.data_manager.factory.readers.ros2_reader.get_data import DataGrabber
 
 
 @dataclass
 class Iterator:
     """Iterator for the sensor`s timestamp file."""
 
-    file: str
+    bagpath:Path
+    msg_limit:int = 10000
     position: int = 0
-    iterator: iter = field(init=False)
+    data: list = None
+
 
     def reset(self):
         self.position = 0
-        self.__post_init__()
+        self._get_data(self.position)
 
-    def __post_init__(self):
-        f = open(self.file, "r")
-        self.iterator = iter(f)
 
-    def __next__(self):
-        try:
-            values = next(self.iterator)
+    def _get_data(self, pos:int):
+        return DataGrabber.iter_rosbag(self.bagpath, pos, self.msg_limit)
+
+
+    def next(self):
             self.position += 1
-            return values
-        except StopIteration:
-            raise
+            return self._get_data(self.position)
 
-    def __iter__(self):
-        return self.iterator
+
+
+if __name__ == "__main__":
+
+    folder_path = Path(os.environ["DATA_DIR"])
+    bag_path = Path("{}/rosbag2_2023_11_02-12_18_16".format(folder_path))
+    my_iterator = Iterator(bag_path)
+    for i in range(20):
+        print(my_iterator.next())
+
+        if i == 10:
+            my_iterator.reset()
+            print("Resetting the iterator")
