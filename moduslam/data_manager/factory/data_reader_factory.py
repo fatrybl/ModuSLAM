0c95599a92caff4984c329cfbe57a20fe2b676e6
FelipeZerokun <epilefrojasleon@gmail.com>
2024-06-13 10:27:02 +0200

    Adding parameters needed for the Ros2 datareader config

--- moduslam/data_manager/factory/data_reader_factory.py
+++ moduslam/data_manager/factory/data_reader_factory.py
@@ -3,12 +3,16 @@ from typing import cast
 
 from moduslam.data_manager.factory.data_reader_ABC import DataReader
 from moduslam.data_manager.factory.readers.kaist.kaist_reader import KaistReader
+from moduslam.data_manager.factory.readers.ros2.ros2_reader import Ros2DataReader
 from moduslam.logger.logging_config import data_manager
 from moduslam.system_configs.data_manager.batch_factory.datasets.base_dataset import (
     DatasetConfig,
 )
 from moduslam.system_configs.data_manager.batch_factory.datasets.kaist.config import (
     KaistConfig,
+)
+from moduslam.system_configs.data_manager.batch_factory.datasets.ros2.config import (
+    Ros2Config,
 )
 from moduslam.system_configs.data_manager.batch_factory.regime import (
     RegimeConfig,
@@ -52,6 +56,9 @@ class DataReaderFactory:
             case KaistReader.__name__:
                 dataset_cfg = cast(KaistConfig, dataset_cfg)
                 return KaistReader(regime, dataset_cfg)
+            # TODO: Add ROS2 reader here.
+            case Ros2DataReader.__name__:
+                dataset_cfg = cast(Ros2Config)
             case _:
                 msg = f"No DataReader exists for dataset type {dataset_cfg.reader!r}."
                 logger.critical(msg)
--- moduslam/setup_manager/config_validator.py
+++ moduslam/setup_manager/config_validator.py
@@ -6,6 +6,9 @@ from moduslam.logger.logging_config import main_manager
 from moduslam.system_configs.data_manager.batch_factory.datasets.kaist.config import (
     KaistConfig,
 )
+from moduslam.system_configs.data_manager.batch_factory.datasets.ros2.config import (
+    Ros2Config,
+)
 from moduslam.system_configs.data_manager.batch_factory.regime import RegimeConfig
 from moduslam.system_configs.frontend_manager.handlers.lidar_odometry import (
     KissIcpScanMatcherConfig,
@@ -28,6 +31,8 @@ def register_config():
     cs = ConfigStore.instance()
     cs.store(name="structured_schema_config", node=MainManagerConfig)
     cs.store(group="datasets", name="base_kaist_dataset", node=KaistConfig)
+    # TODO: Include/replace dataset configs here.
+    cs.store(group="datasets", name="base_ros2_dataset", node=Ros2Config)
     cs.store(group="regimes", name="base_regime", node=RegimeConfig)
     cs.store(group="handlers", name="base_kiss_icp_odometry", node=KissIcpScanMatcherConfig)
     cs.store(group="handlers", name="base_vrs_gps_preprocessor", node=VrsGpsHandlerConfig)
