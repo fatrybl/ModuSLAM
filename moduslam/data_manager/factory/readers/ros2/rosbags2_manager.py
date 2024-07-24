a0f35591cfcc844b30b4dab56707317f3bfb0b45
FelipeZerokun <epilefrojasleon@gmail.com>
2024-07-24 11:55:41 +0200

    Re structuring ros2bags manager for new sensors_table and new measurement_collector class

--- moduslam/data_manager/factory/readers/ros2/rosbags2_manager.py
+++ moduslam/data_manager/factory/readers/ros2/rosbags2_manager.py
@@ -15,7 +15,9 @@ class Rosbags2Manager:
         self.bag_path: Path = bag_path
         self.iterator = Iterator()
         self.sensors: dict = sensors_table
-        self.sensors_list = self._map_sensors()
+        self.sensors_list, self.topics_list = self._map_sensors()
+        self.connections = self._get_connections()
+        # TODO: depending on the sensors configuration, change connections
         if time_range is not None:
             self.start = time_range.start
             self.stop = time_range.stop
@@ -41,7 +43,7 @@ class Rosbags2Manager:
                     "id": connection.id,
                     "topic": connection.topic,
                     "message_type": connection.msgtype,
-                    "sensor_name": sensor_name,
+                    "sensor": sensor_name,
                     "data_type": data_type,
                 }
                 sensors.append(sensor)
@@ -49,26 +51,32 @@ class Rosbags2Manager:
 
     def _map_sensors(self):
         print("Mapping topics to sensors")
+        updated_list = []
+        connections_list = []
         sensors_list = self._get_topics()
         for k, v in self.sensors.items():
             for sensor in sensors_list:
-                if sensor["sensor_name"] in v:
-                    sensor["sensor_type"] = k
+                if sensor["sensor"] == k:
+                    sensor["sensor_name"] = v
+                    updated_list.append(sensor)
+                    connections_list.append(sensor["topic"])
+                    continue
+        return updated_list, connections_list
 
-        return sensors_list
+    def _get_connections(self):
 
-    def rosbag_read(self, num_readings: int = 1, topic_name: str = None) -> list:
+        with Reader(self.bag_path) as reader:
+            connections = [c for c in reader.connections if c.topic in self.topics_list]
+        return connections
+
+    def rosbag_read(self, num_readings: int = 1) -> list:
 
         n = 1
         with Reader(self.bag_path) as reader:
-            if topic_name == None:
-                connections = [c for c in reader.connections]
-            else:
-                connections = [c for c in reader.connections if c.topic == topic_name]
-
-            for connection, timestamp, rawdata in reader.messages(connections=connections):
+            for connection, timestamp, rawdata in reader.messages(connections=self.connections):
                 print(f"Reading messages from {self.start} to {self.stop}")
                 if n == num_readings:
+                    # TODO: clean this part into separate functions
                     msg = deserialize_cdr(rawdata, connection.msgtype)
                     dt_obj = int(str(timestamp)[0:10])
                     nano_seconds = int(str(timestamp)[10:])
@@ -90,9 +98,8 @@ class Rosbags2Manager:
 
     def next_sensor_read(self):
         n = 1
-
         with Reader(self.bag_path) as reader:
-            for connection, timestamp, rawdata in reader.messages():
+            for connection, timestamp, rawdata in reader.messages(connections=self.connections):
                 if n == self.iterator.get_iter():
                     if timestamp < self.start:
                         print(f"timestamp {timestamp} is not yet in {self.start}")
@@ -103,17 +110,32 @@ class Rosbags2Manager:
                         print(f"reached the stop timestamp {self.stop}")
                         return None
 
-                    msg = deserialize_cdr(rawdata, connection.msgtype)
-                    dt_obj = int(str(timestamp)[0:10])
-                    nano_seconds = int(str(timestamp)[10:])
-                    # print(dt_obj, nano_seconds)
-                    data = (n, connection.id, connection.topic, connection.msgtype, timestamp)
+                    print(type(rawdata))
+
+                    msg = self.get_data(rawdata, connection.msgtype)
                     data = (connection.id, n, msg, timestamp)
                     self.iterator.next()
                     return data
                 else:
                     n += 1
             return None
+
+    def get_date_time(self, timestamp):
+        date_obj = int(str(timestamp)[0:10])
+        nano_seconds = int(str(timestamp)[10:])
+        return date_obj, nano_seconds
+
+    def get_data(self, raw_data, message_type):
+        if message_type == "sensor_msgs/msg/Imu":
+            msg = self.get_imu_data(raw_data, message_type)
+            print("THIS IS THE MESSAGE CONTENT")
+            print(msg.orientation)
+            print(msg.angular_velocity)
+            print(msg.linear_acceleration)
+        if message_type == "sensor_msgs/msg/Image":
+            msg = self.get_camera_data(raw_data, message_type)
+
+        return msg
 
 
 if __name__ == "__main__":
