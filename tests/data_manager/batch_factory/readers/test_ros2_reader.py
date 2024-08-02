7c9104d8a301ae85d02369a26aed05446bc436d9
FelipeZerokun <epilefrojasleon@gmail.com>
2024-08-02 07:17:50 +0200

    Created test_ros2_reader.py for testing the creation of the Ros2Reader object. three tests: 1) Creation of Ros2Reader object with correct directory 2) Creation of Ros2Reader object and test of the Reader usage using Context Manager With 3) Try to create Ros2Reader object with invalid directory

---
+++ tests/data_manager/batch_factory/readers/test_ros2_reader.py
@@ -0,0 +1,47 @@
+"""Tests for Ros2Reader constructor.
+
+1) test_ros2_reader_1: Successful creation.
+2) test_ros2_reader_2: Successful creation.
+creation and exploration of rosbag
+3) test_ros2_reader_3: Unsuccessful
+creation with non-existent directory
+"""
+
+from pathlib import Path
+
+import pytest
+
+from moduslam.data_manager.batch_factory.readers.ros2.reader import Ros2DataReader
+from moduslam.system_configs.data_manager.batch_factory.datasets.ros2.config import (
+    Ros2Config,
+)
+from moduslam.system_configs.data_manager.batch_factory.regimes import Stream
+from tests.conftest import ros2_dataset_dir
+
+
+def test_ros2_reader_1():
+    print(ros2_dataset_dir)
+    dataset_cfg = Ros2Config(directory=ros2_dataset_dir)
+    reader = Ros2DataReader(regime=Stream(), dataset_params=dataset_cfg)
+    assert reader is not None
+
+
+def test_ros2_reader_2():
+    dataset_cfg = Ros2Config(directory=ros2_dataset_dir)
+    limit_check = 20
+    with Ros2DataReader(regime=Stream(), dataset_params=dataset_cfg) as reader:
+        assert reader is not None
+        print("Rosbag opened successfully")
+        n = 0
+        for connection in reader.connections:
+            n += 1
+            print(f"Topic: {connection.topic}, Type: {connection.msgtype}")
+
+    print("Rosbag closed successfully")
+
+
+def test_ros2_reader_3():
+    dataset_cfg = Ros2Config(directory=Path("some/invalid/dataset/path"))
+
+    with pytest.raises(NotADirectoryError):
+        Ros2DataReader(regime=Stream(), dataset_params=dataset_cfg)
