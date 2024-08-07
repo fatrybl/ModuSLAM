bad9c1f406f372af238e750c13bbab99aa84a11b
FelipeZerokun <epilefrojasleon@gmail.com>
2024-08-07 12:43:57 +0200

    Helping functions for data extractions from messages got using rosbags.reader Class

---
+++ moduslam/data_manager/batch_factory/readers/ros2/measurement_collector.py
@@ -0,0 +1,13 @@
+# TODO: Define data extracting functions for each sensors in a Rosbag
+
+
+def get_stereo_measurement():
+    pass
+
+
+def get_imu_measurement():
+    pass
+
+
+def get_lidar_measurement():
+    pass
