685f36185939dea9576326d0129e5d88d8e755ea
FelipeZerokun <epilefrojasleon@gmail.com>
2024-08-14 12:42:28 +0200

    Returning image data as array for the get_stereo_measurement function

--- moduslam/data_manager/batch_factory/readers/ros2/measurement_collector.py
+++ moduslam/data_manager/batch_factory/readers/ros2/measurement_collector.py
@@ -1,13 +1,15 @@
 # TODO: Define data extracting functions for each sensors in a Rosbag
 
 
-def get_stereo_measurement():
+def get_stereo_measurement(raw_msg):
+    return raw_msg.data
+
+    # TODO: Change format from array to PIL Image
+
+
+def get_imu_measurement(raw_msg):
     pass
 
 
-def get_imu_measurement():
-    pass
-
-
-def get_lidar_measurement():
+def get_lidar_measurement(raw_msg):
     pass
