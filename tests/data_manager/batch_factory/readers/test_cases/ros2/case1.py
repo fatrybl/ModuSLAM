53a5df64727a2ef3cf371439df82f6b84c5b2341
FelipeZerokun <epilefrojasleon@gmail.com>
2024-09-22 19:32:31 +0200

    using test_rosbag_100 for creating test elements.

--- tests/data_manager/batch_factory/readers/test_cases/ros2/case1.py
+++ tests/data_manager/batch_factory/readers/test_cases/ros2/case1.py
@@ -9,75 +9,91 @@ from tests.conftest import ros2_dataset_dir
 from tests_data_generators.ros2_dataset.data import elements
 from tests_data_generators.utils import generate_sensors_factory_config
 
-timestamp1 = 1698927496694557229
-timestamp2 = 1698927497652542865
-timestamp3 = 1698927498123884925
-timestamp4 = 1698927499116096802
-timestamp5 = 1698927499980874274
+el1 = elements[0]
+el2 = elements[1]
+el3 = elements[2]
+el4 = elements[3]
+el5 = elements[4]
+el6 = elements[5]
+el7 = elements[6]
+el8 = elements[7]
+el9 = elements[8]
+el10 = elements[9]
+el11 = elements[10]
+el12 = elements[11]
+el13 = elements[12]
+el14 = elements[13]
+el15 = elements[14]
+el16 = elements[15]
+el17 = elements[16]
+el18 = elements[17]
+el19 = elements[18]
+el20 = elements[19]
+el21 = elements[20]
+el22 = elements[21]
+el23 = elements[22]
+el24 = elements[23]
+el25 = elements[24]
+el26 = elements[25]
+el27 = elements[26]
+el28 = elements[27]
+el29 = elements[28]
+el30 = elements[29]
+el31 = elements[30]
+el32 = elements[31]
+el33 = elements[32]
+el34 = elements[33]
+el35 = elements[34]
+el36 = elements[35]
+el37 = elements[36]
+el38 = elements[37]
+el39 = elements[38]
+el40 = elements[39]
+el41 = elements[40]
+el42 = elements[41]
+el43 = elements[42]
+el44 = elements[43]
+el45 = elements[44]
+el46 = elements[45]
+el47 = elements[46]
+el48 = elements[47]
+el49 = elements[48]
+el50 = elements[49]
+el51 = elements[50]
+el52 = elements[51]
+el53 = elements[52]
+el54 = elements[53]
+
+
+timestamp1 = 1698927496694033807
+timestamp2 = 1698927496739239954  # 20 sensor readings
+timestamp3 = 1698927496799306816  # 40 sensor readings
+timestamp4 = 1698927496898641344  # 60 sensor readings
+timestamp5 = 1698927497046583719  # 80 sensor readings
+timestamp5 = 1698927497190095250  # 100 sensor readings
 
 timelimit1 = TimeLimit(start=timestamp1, stop=timestamp5)
-timelimit2 = TimeLimit(start=timestamp2, stop=timestamp5)
-timelimit3 = TimeLimit(start=timestamp1, stop=timestamp4)
-timelimit4 = TimeLimit(start=timestamp2, stop=timestamp4)
-timelimit5 = TimeLimit(start=timestamp3, stop=timestamp5)
 
 sensors_table1 = {
     "stereo_camera_left": "left",
     "stereo_camera_right": "right",
-    "imu": "imu",
+    "imu": "xsens",
     "lidar_left": "vlp16l",
     "lidar_right": "vlp16r",
     "lidar_center": "vlp32c",
 }
 
-sensors_table2 = {
-    "stereo_camera_left": "left",
-    "imu": "imu",
-    "lidar_center": "vlp32c",
-}
-
-sensors_table3 = {
-    "stereo_camera_left": "left",
-    "stereo_camera_right": "right",
-}
-
-sensors_table4 = {
-    "imu": "imu",
-}
-
-sensors_table5 = {
-    "lidar_left": "vlp16l",
-    "lidar_right": "vlp16r",
-    "lidar_center": "vlp32c",
-}
 
 sensors1 = [Sensor(SensorConfig(sensor_name)) for sensor_name in list(sensors_table1.keys())]
-sensors2 = [Sensor(SensorConfig(sensor_name)) for sensor_name in list(sensors_table2.values())]
-sensors3 = [Sensor(SensorConfig(sensor_name)) for sensor_name in list(sensors_table3.values())]
-sensors4 = [Sensor(SensorConfig(sensor_name)) for sensor_name in list(sensors_table4.values())]
-sensors5 = [Sensor(SensorConfig(sensor_name)) for sensor_name in list(sensors_table5.values())]
 
 print(sensors1)
 
 dataset_cfg1 = Ros2Config(directory=ros2_dataset_dir, sensors_table=sensors_table1)
-dataset_cfg2 = Ros2Config(directory=ros2_dataset_dir, sensors_table=sensors_table2)
-dataset_cfg3 = Ros2Config(directory=ros2_dataset_dir, sensors_table=sensors_table3)
-dataset_cfg4 = Ros2Config(directory=ros2_dataset_dir, sensors_table=sensors_table4)
-dataset_cfg5 = Ros2Config(directory=ros2_dataset_dir, sensors_table=sensors_table5)
 
 stream = Stream()
 
 sensors_factory_config1 = generate_sensors_factory_config(sensors1)
-sensors_factory_config2 = generate_sensors_factory_config(sensors2)
-sensors_factory_config3 = generate_sensors_factory_config(sensors3)
-sensors_factory_config4 = generate_sensors_factory_config(sensors4)
-sensors_factory_config5 = generate_sensors_factory_config(sensors5)
 
-print("List of configs for sensors")
-print(sensors_factory_config1)
-
-print("elements are:")
-print(elements)
 
 incorrect_sensors_factory_config = generate_sensors_factory_config([])
 
