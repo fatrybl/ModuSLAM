5a57450ae7616d38d4b3738a8ac5a7f38819977c
FelipeZerokun <epilefrojasleon@gmail.com>
2024-10-02 20:28:06 +0200

    Fixing bad Pull from develop branch

--- moduslam/setup_manager/manager.py
+++ moduslam/setup_manager/manager.py
@@ -20,11 +20,6 @@ class SetupManager:
         Args:
             config: configuration of the SetupManager.
         """
-        print("CONFIGURATION FOR SENSORS IS:")
-        for sensors in config.sensors_factory.values():
-            for sensor, value in sensors.items():
-                print(sensor, value)
-
         SensorsFactory.init_sensors(config.sensors_factory)
         HandlersFactory.init_handlers(config.handlers_factory)
         EdgeFactoriesInitializer.init_factories(config.edge_factories_initializer)
