8030b3b5bfa3caff10bdcf14fa0148b3a523ff15
FelipeZerokun <epilefrojasleon@gmail.com>
2024-10-04 15:26:12 +0200

    Added staticmethod for ros2_regime

--- moduslam/data_manager/batch_factory/readers/regime_factory.py
+++ moduslam/data_manager/batch_factory/readers/regime_factory.py
@@ -62,3 +62,30 @@ class Factory:
             case _:
                 msg = "Invalid regime name in config."
                 raise ValueError(msg)
+
+    @staticmethod
+    def ros2_regime(regime_config: DataRegimeConfig) -> Stream | TimeLimit:
+        """Creates a regime for ROS2 Dataset reader.
+
+        Args:
+            regime_config: config with data flow regime.
+
+        Returns:
+            a regime.
+
+        Raises:
+            ValueError: if a regime name in config is invalid.
+        """
+
+        match regime_config.name:
+            case Stream.name:
+                return Stream()
+
+            case TimeLimit.name:
+                start = to_int(regime_config.start)
+                stop = to_int(regime_config.stop)
+                return TimeLimit(start, stop)
+
+            case _:
+                msg = "Invalid regime name in config."
+                raise ValueError(msg)
