361851ebaf88460394b28b0ca3ca3a64a6243aa1
FelipeZerokun <epilefrojasleon@gmail.com>
2024-10-04 15:24:53 +0200

    changed ros2 tuple name to avoid confusion

--- tests/data_manager/batch_factory/readers/test_get_next_element.py
+++ tests/data_manager/batch_factory/readers/test_get_next_element.py
@@ -32,11 +32,11 @@ from moduslam.system_configs.setup_manager.sensor_factory import SensorsFactoryC
 from moduslam.utils.auxiliary_methods import equal_elements
 from tests.data_manager.batch_factory.readers.test_cases.kaist.case1 import kaist1
 from tests.data_manager.batch_factory.readers.test_cases.kaist.case2 import kaist2
-from tests.data_manager.batch_factory.readers.test_cases.ros2.case1 import ros1
+from tests.data_manager.batch_factory.readers.test_cases.ros2.case1 import ros2_case1
 from tests.data_manager.batch_factory.readers.test_cases.tum_vie.case1 import tum_vie1
 from tests.data_manager.batch_factory.readers.test_cases.tum_vie.case2 import tum_vie2
 
-test_cases_1 = (*kaist1, *tum_vie1, *ros1)
+test_cases_1 = (*kaist1, *tum_vie1, *ros2_case1)
 test_cases_2 = (*kaist2, *tum_vie2)
 
 
