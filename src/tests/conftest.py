cbd6d2a4e1a690c69f6d4204567d50c719d47ada
Mark Griguletskii <mark.griguletskii@skoltech.ru>
2025-01-05 16:30:29 +0300

    FIX: tests runner
    Signed-off-by: Mark Griguletskii <mark.griguletskii@skoltech.ru>

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,14 @@
 import os
 from pathlib import Path
 
-env_var: str | None = os.getenv("TEST_DATA_DIR")
-
-print(f"MY ENV VAAAR: {env_var} ")
+env_var: str | None = os.getenv("SRC_DIR")
 
 if env_var is not None:
-    test_data_dir = Path(env_var)
+    src_dir = Path(env_var)
 else:
-    root_dir = Path(__file__).resolve().parent.parent
-    test_data_dir = root_dir / "tests_data"
+    src_dir = Path(__file__).resolve().parent.parent
+
+test_data_dir = src_dir / "tests_data"
 
 kaist_custom_dataset_dir = test_data_dir / "kaist_urban_custom_dataset"
 kaist_urban30_dataset_dir = test_data_dir / "datasets" / "kaist_urban/kaist_urban30_gangnam"
