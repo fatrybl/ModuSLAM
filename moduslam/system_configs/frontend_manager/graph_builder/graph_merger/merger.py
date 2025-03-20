11df127a1d2ab80c7cb0b2843d82f79ac9dae325
Elzaaa <elza.shakirova1307@gmail.com>
2025-03-20 14:37:08 +0100

    Clean up old folders and sync structure

--- moduslam/system_configs/frontend_manager/graph_builder/graph_merger/merger.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class GraphMergerConfig:
-    """Base graph merger configuration."""
-
-    handler_edge_factory_table: dict[str, str]
--- moduslam/system_configs/map_manager/map_types.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
-class MapType:
-    lidar_pointcloud: str = "lidar_pointcloud"
-    camera_pointcloud: str = "camera_pointcloud"
-    trajectory: str = "trajectory"
