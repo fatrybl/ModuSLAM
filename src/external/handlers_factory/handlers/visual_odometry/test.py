5d5c55e2ced8c2b14b2eafd26c4e3ae769ab1755
fatrybl <fatrybl@yandex.ru>
2025-04-22 14:53:52 +0300

    DEL: unnecessary files , sync branches
    Signed-off-by: Mark Griguletskii <mark.griguletskii@skoltech.ru>
    Co-authored-by: Mark Griguletskii <mark.griguletskii@skoltech.ru>

--- src/external/handlers_factory/handlers/visual_odometry/test.py
+++
@@ -1,9 +0,0 @@
-import scipy.io
-
-# Load the .mat file
-mat_file_path = "/media/mark/WD/kaist/urban-26/calibration/stereoParams.mat"
-calibration_data = scipy.io.loadmat(mat_file_path)
-
-# Access the calibration data
-# Assuming the calibration data is stored under the key 'calibration'
-print("Camera Matrix:\n", calibration_data)
